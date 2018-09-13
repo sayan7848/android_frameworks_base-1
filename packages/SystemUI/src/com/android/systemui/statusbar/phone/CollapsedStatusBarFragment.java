8a4cf033400f46e21b1148989b0495b978083551
maxwen <max.weninger@gmail.com>
2018-09-13 14:47:41 +0200

    base: SystemUI: tuner: fix hiding of statusbar clock
    clock in expanded quick settings will not be hidden
    from tuner setting
    
    Change-Id: Ic5db96fba79e5bc77282b70398e7f8055b998892

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
@@ -44,6 +44,7 @@ import com.android.systemui.SysUiServiceProvider;
 import com.android.systemui.statusbar.CommandQueue;
 import com.android.systemui.statusbar.phone.StatusBarIconController.DarkIconManager;
 import com.android.systemui.statusbar.phone.TickerView;
+import com.android.systemui.statusbar.policy.Clock;
 import com.android.systemui.statusbar.policy.DarkIconDispatcher;
 import com.android.systemui.statusbar.policy.EncryptionHelper;
 import com.android.systemui.statusbar.policy.KeyguardMonitor;
@@ -66,7 +67,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     private KeyguardMonitor mKeyguardMonitor;
     private NetworkController mNetworkController;
     private LinearLayout mSystemIconArea;
-    private View mClockView;
+    private Clock mClockView;
     private View mRightClock;
     private int mClockStyle;
     private View mNotificationIconAreaInner;
@@ -150,7 +151,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         mDarkIconManager.setShouldLog(true);
         Dependency.get(StatusBarIconController.class).addIconGroup(mDarkIconManager);
         mSystemIconArea = mStatusBar.findViewById(R.id.system_icon_area);
-        mClockView = mStatusBar.findViewById(R.id.clock);
+        mClockView = (Clock) mStatusBar.findViewById(R.id.clock);
         mCenterClockLayout = (LinearLayout) mStatusBar.findViewById(R.id.center_clock_layout);
         mRightClock = mStatusBar.findViewById(R.id.right_clock);
         mLiquidLogo = (ImageView) mStatusBar.findViewById(R.id.status_bar_logo);
@@ -263,17 +264,21 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
 
     public void hideSystemIconArea(boolean animate) {
         animateHide(mSystemIconArea, animate, true);
-        animateHide(mCenterClockLayout, animate, true);
-        if (mClockStyle == 2) {
-            animateHide(mRightClock, animate, true);
+        if (mClockView.isClockVisible()) {
+            animateHide(mCenterClockLayout, animate, true);
+            if (mClockStyle == 2) {
+                animateHide(mRightClock, animate, true);
+            }
         }
     }
 
     public void showSystemIconArea(boolean animate) {
         animateShow(mSystemIconArea, animate);
-        animateShow(mCenterClockLayout, animate);
-        if (mClockStyle == 2) {
-            animateShow(mRightClock, animate);
+        if (mClockView.isClockVisible()) {
+            animateShow(mCenterClockLayout, animate);
+            if (mClockStyle == 2) {
+                animateShow(mRightClock, animate);
+            }
         }
     }
 
@@ -282,7 +287,9 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         if (mShowLogo) {
             animateHide(mLiquidLogo, animate, true);
         }
-        animateHide(mCenterClockLayout, animate, true);
+        if (mClockView.isClockVisible()) {
+            animateHide(mCenterClockLayout, animate, true);
+        }
     }
 
     public void showNotificationIconArea(boolean animate) {
@@ -290,7 +297,9 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         if (mShowLogo) {
             animateShow(mLiquidLogo, animate);
         }
-        animateShow(mCenterClockLayout, animate);
+        if (mClockView.isClockVisible()) {
+            animateShow(mCenterClockLayout, animate);
+        }
     }
 
     public void hideOperatorName(boolean animate) {
@@ -404,6 +413,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     }
 
     private void updateClockStyle(boolean animate) {
+        if (!mClockView.isClockVisible()) return;
         if (mClockStyle == 1 || mClockStyle == 2) {
             animateHide(mClockView, animate, false);
         } else {
