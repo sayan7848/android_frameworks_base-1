b1611b131e16a1c817ebba847c0775295fab7f60
Mingyan Liu <mingyan.liu@spreadtrum.com>
2018-09-28 18:31:38 +0800

    Revert "ViewRoot: Ensure we release surface from setWindowStopped"
    This reverts commit 6858bb9ab1e867245370a8edf9e348ddc5ceec31.
    
    The patch may cause ViewRoot cannot draw because surface is invalid at some times.
    
    Bug: 116683875
    Test: Set screen lock to None,goto any listactivty, turn off/turn on screen, slide the list of Settings app, check whether the list will scroll down or up.
    Test: Set screen lock to None,show any progress dialog.Turn off/turn on the screen, check whether progressbar updates.
    
    Change-Id: Id80c09c36ddec3cbbe92a87ff7f3255de84bede6
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- core/java/android/view/ViewRootImpl.java
+++ core/java/android/view/ViewRootImpl.java
@@ -1394,10 +1394,6 @@ public final class ViewRootImpl implements ViewParent,
             for (int i = 0; i < mWindowStoppedCallbacks.size(); i++) {
                 mWindowStoppedCallbacks.get(i).windowStopped(stopped);
             }
-
-            if (mStopped) {
-                mSurface.release();
-            }
         }
     }
 
