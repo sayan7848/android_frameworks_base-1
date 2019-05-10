16705bd41e41790d14554a6e5ab8332434d5843e
sayan7848 <sayan7848@gmail.com>
2019-05-10 21:05:49 +0000

    Revert "Initial support for OnePlus in-display fingerprint sensor"
    This reverts commit 339db83e8e07fcd986f1538c6145b749e7f75288.
    
    Signed-off-by: sayan7848 <sayan7848@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -103,7 +103,6 @@ public class CommandQueue extends IStatusBar.Stub {
     private static final int MSG_TOGGLE_FLASHLIGHT             = 53 << MSG_SHIFT;
     private static final int MSG_TOGGLE_NAVIGATION_EDITOR      = 54 << MSG_SHIFT;
     private static final int MSG_DISPATCH_NAVIGATION_EDITOR_RESULTS = 55 << MSG_SHIFT;
-    private static final int MSG_IN_DISPLAY_FINGERPRINT        = 56 << MSG_SHIFT;
 
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
@@ -176,7 +175,6 @@ public class CommandQueue extends IStatusBar.Stub {
         default void onFingerprintHelp(String message) { }
         default void onFingerprintError(String error) { }
         default void hideFingerprintDialog() { }
-        default void handleInDisplayFingerprintView(boolean show, boolean isEnrolling) { }
         default void toggleCameraFlash() { }
         default void setAutoRotate(boolean enabled) { }
         default void restartUI() { }
@@ -618,17 +616,6 @@ public class CommandQueue extends IStatusBar.Stub {
     }
 
     @Override
-    public void handleInDisplayFingerprintView(boolean show, boolean isEnrolling) {
-        synchronized (mLock) {
-            SomeArgs args = SomeArgs.obtain();
-            args.arg1 = show;
-            args.arg2 = isEnrolling;
-            mHandler.obtainMessage(MSG_IN_DISPLAY_FINGERPRINT, args)
-                    .sendToTarget();
-        }
-    }
-
-    @Override
     public void toggleCameraFlash() {
         synchronized (mLock) {
             mHandler.removeMessages(MSG_TOGGLE_CAMERA_FLASH);
@@ -882,14 +869,6 @@ public class CommandQueue extends IStatusBar.Stub {
                     for (int i = 0; i < mCallbacks.size(); i++) {
                         mCallbacks.get(i).hideFingerprintDialog();
                     }
-                    break;
-                case MSG_IN_DISPLAY_FINGERPRINT:
-                    for (int i = 0; i < mCallbacks.size(); i++) {
-                        mCallbacks.get(i).handleInDisplayFingerprintView(
-                                (boolean)((SomeArgs)msg.obj).arg1,
-                                (boolean)((SomeArgs)msg.obj).arg2);
-                    }
-
                     break;
                 case MSG_SHOW_CHARGING_ANIMATION:
                     for (int i = 0; i < mCallbacks.size(); i++) {
